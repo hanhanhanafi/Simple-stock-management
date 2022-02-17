99d9d391f7531120dbd1c4fe0c49c557c82b8097
Hanhan Hanafi <53867533+hanhanhanafi@users.noreply.github.com>
2022-02-17 13:34:35 +0700

    Menambahkan Function menampilkan data

--- function.php
+++ function.php
@@ -3,3 +3,16 @@
 session_start();
 
 $connectdb = mysqli_connect("localhost", "root", "", "stockbarang");
+
+if (isset($_POST['newbarang'])) {
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+    $inputtabel = mysqli_query($connectdb, "insert into stock (namabarang, deskripsi, stock) values('$namabarang','$deskripsi','$stock')");
+    if ($inputtabel) {
+        header('location:index.php');
+    } else {
+     echo "Gagal Menambah Data";
+    }
+}
