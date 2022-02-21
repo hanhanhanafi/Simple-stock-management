73525893dcb9dfffdc868411a594cc496a596044
Hanhan Hanafi <53867533+hanhanhanafi@users.noreply.github.com>
2022-02-21 09:56:21 +0700

    Menambahkan function edit dan hapus data

--- function.php
+++ function.php
@@ -16,3 +16,34 @@ if (isset($_POST['newbarang'])) {
      echo "Gagal Menambah Data";
     }
 }
+
+// edit barang
+
+if (isset($_POST['editbarang'])) {
+    $idbarang = $_POST['idbarang'];
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+$edit = mysqli_query($connectdb, "UPDATE stock SET namabarang='$namabarang', deskripsi='$deskripsi', stock='$stock' WHERE idbarang='$idbarang'");
+if ($edit) {
+    header('location:index.php');
+} else {
+ echo "Gagal Menambah Data";
+}
+}
+
+// Hapus Barang
+
+if (isset($_POST['hapusbarang'])) {
+    $idbarang = $_POST['idbarang'];
+
+    $hapus = mysqli_query($connectdb, "DELETE FROM stock WHERE idbarang='$idbarang'")or die(mysqli_error());
+    if ($hapus) {
+        header('location:index.php');
+    } else {
+     echo "Gagal Menghapus Data";
+    }
+}
+
+?>
